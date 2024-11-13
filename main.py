221fe75d17e0506d026280a22f6f2b51789e8df5
Asliddin Abdumannonov <asliddinabdumannonov06@gmail.com>
2024-11-13 18:22:20 +0500

    Added Routing

--- main.py
+++ main.py
@@ -1,3 +1,12 @@
 from app import PyRomApp
 
 app = PyRomApp()
+
+@app.route("/home")
+def home(request, response):
+    response.text = "Hello from Home Page"
+
+@app.route("/about")
+def about(request, response):
+    response.text = "Hello from About Page"
+
