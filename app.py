5165dc45a83d52fee38793f259e6f3b17600bf0a
Asliddin Abdumannonov <asliddinabdumannonov06@gmail.com>
2024-11-14 10:15:07 +0500

    Modified to Class based Handlers

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from webob import Request, Response
 from parse import parse
+import inspect
 
 class PyRomApp:
 
@@ -17,9 +18,18 @@ class PyRomApp:
         handler, kwargs = self.find_handler(request)
 
         if handler is not None:
+            if inspect.isclass(handler):
+                handle = getattr(handler(), request.method.lower(), None)
+
+                if handler is None:
+                    response.status_code = 405
+                    response.text = "Method Not Allowed"
+                    return response
+                
             handler(request, response, **kwargs)
         else:
             self.default_response(response)
+
         return response
 
     def find_handler(self, request):
@@ -35,6 +45,9 @@ class PyRomApp:
         response.text = "Not Found"
     
     def route(self, path):
+        
+        assert path not in self.routes, "Duplicate route. PLease change the URL"
+
         def wrapper(handler):
             self.routes[path] = handler
             return handler
